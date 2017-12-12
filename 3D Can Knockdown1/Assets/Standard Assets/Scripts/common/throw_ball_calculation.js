b6e7754b83d81ebc0155a165e70e654dbabc1493
Or Shtrauch <ororor2000@gmail.com>
2017-12-12 17:26:18 +0200

    Or 12/12/2017 1726

--- 3D Can Knockdown1/Assets/Standard Assets/Scripts/common/throw_ball_calculation.js
+++ 3D Can Knockdown1/Assets/Standard Assets/Scripts/common/throw_ball_calculation.js
@@ -18,135 +18,135 @@ var rotationSpeed : float = 100;
 var hittingbb:float=0.0;
 function Start()
 {
-timer=0;
-islaunch_ball=false;
+    timer=0;
+    islaunch_ball=false;
 }
 
 function Update() {
 
 
-if(onbackpress.is_back_press==false){
-var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    if(onbackpress.is_back_press==false){
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
  
-var hit : RaycastHit;
-if (Physics.Raycast(ray.origin, ray.direction, hit))
-{
+        var hit : RaycastHit;
+        if (Physics.Raycast(ray.origin, ray.direction, hit))
+        {
 
 
-// Debug.Log("no=  "+islaunch_ball);
-if(islaunch_ball)
-{
-timer+=Time.deltaTime;
-if(hit.collider.tag == ("newball"))
-GameObject.Find("new_ball").transform.position.x=hit.point.x;
+            // Debug.Log("no=  "+islaunch_ball);
+            if(islaunch_ball)
+            {
+                timer+=Time.deltaTime;
+                if(hit.collider.tag == ("newball"))
+                    GameObject.Find("new_ball").transform.position.x=hit.point.x;
 
 
 
-var hitting = hit.point.x;
-	//Debug.Log("hitting"+hitting);
-	if(hitting <hittingbb){
-	GameObject.Find("new_ball").transform.Rotate(Vector3(0,0,rotationSpeed* Time.deltaTime));
-	}
-	if(hitting >hittingbb){
-	GameObject.Find("new_ball").transform.Rotate(Vector3(0,0,-rotationSpeed* Time.deltaTime));
-	}
+                var hitting = hit.point.x;
+                //Debug.Log("hitting"+hitting);
+                if(hitting <hittingbb){
+                    GameObject.Find("new_ball").transform.Rotate(Vector3(0,0,rotationSpeed* Time.deltaTime));
+                }
+                if(hitting >hittingbb){
+                    GameObject.Find("new_ball").transform.Rotate(Vector3(0,0,-rotationSpeed* Time.deltaTime));
+                }
 	
-	hittingbb = hit.point.x;
-//	Debug.Log("hittingbb"+hittingbb);
+                hittingbb = hit.point.x;
+                //	Debug.Log("hittingbb"+hittingbb);
 
 
-}
-else
-{
-//Debug.Log("timer  ="+timer);
-set_speedandpower();
-timer=0;
-}
+            }
+            else
+            {
+                //Debug.Log("timer  ="+timer);
+                set_speedandpower();
+                timer=0;
+            }
      
-        //Debug.Log("timer  =  "+timer);
+            //Debug.Log("timer  =  "+timer);
         
         }
-   if (Input.GetButtonDown ("Fire1"))
-    {
-        if(hit.collider.tag == ("newball"))
+        if (Input.GetButtonDown ("Fire1"))
         {
+            if(hit.collider.tag == ("newball"))
+            {
         
        
-        click++;
-       // a++;
-           // Application.LoadLevel("introduccion");
-           islaunch_ball=true;
+                click++;
+                // a++;
+                // Application.LoadLevel("introduccion");
+                islaunch_ball=true;
            
-//           Debug.Log("islaunch_ball  ="+islaunch_ball+" click ="+click);
+                //           Debug.Log("islaunch_ball  ="+islaunch_ball+" click ="+click);
 
         
-        }
+            }
         
        
-    }
-    
-     if(Input.GetButtonUp("Fire1"))
-        {
-        
-         if(hit.collider.tag == ("newball")||hit.collider.tag=="ball_stand")
-        {
-        
-       // a++;
-           // Application.LoadLevel("introduccion");
-           islaunch_ball=false;
-      //             Debug.Log("islaunch_ball  ="+islaunch_ball);
-
         }
+    
+        if(Input.GetButtonUp("Fire1"))
+        {
+        
+            if(hit.collider.tag == ("newball")||hit.collider.tag=="ball_stand")
+            {
+        
+                // a++;
+                // Application.LoadLevel("introduccion");
+                islaunch_ball=false;
+                //             Debug.Log("islaunch_ball  ="+islaunch_ball);
+
+            }
     
    
-}
+        }
 
   
 
-}
+    }
 }
 
 function set_speedandpower()
 {
-if(timer<=0.5)
-{
-//if(islaunch_ball)
- // audio.PlayOneShot(swapmediumslow);
-timer_for_next_ball=0.5;
-definepower=250;
-definespeed=15;
-}
-else if(timer>0.5&&timer<=0.7)
-{
-timer_for_next_ball=0.8;
-definepower=140;
-definespeed=25;
-}
-else if(timer>0.7&&timer<=0.9)
-{
-timer_for_next_ball=1;
-definepower=100;
-definespeed=30;
-}
-else if(timer>0.9&&timer<=1.2)
-{
-timer_for_next_ball=1.2;
-definepower=60;
-definespeed=40;
-}
-else if(timer>1.2&&timer<=1.5)
-{
-timer_for_next_ball=1.5;
-definepower=45;
-definespeed=60;
-}
+    if(timer<=0.5)
+    {
+        //if(islaunch_ball)
+        // audio.PlayOneShot(swapmediumslow);
+        timer_for_next_ball=0.5;
+        definepower=250;
+        definespeed=15;
+    }
+    else if(timer>0.5&&timer<=0.7)
+    {
+        timer_for_next_ball=0.8;
+        definepower=140;
+        definespeed=25;
+    }
+    else if(timer>0.7&&timer<=0.9)
+    {
+        timer_for_next_ball=1;
+        definepower=100;
+        definespeed=30;
+    }
+    else if(timer>0.9&&timer<=1.2)
+    {
+        timer_for_next_ball=1.2;
+        definepower=60;
+        definespeed=40;
+    }
+    else if(timer>1.2&&timer<=1.5)
+    {
+        timer_for_next_ball=1.5;
+        definepower=45;
+        definespeed=60;
+    }
 
-else if(timer>1.5)
-{
-timer_for_next_ball=2;
-definepower=32;
-definespeed=80;
-}
+    else if(timer>1.5)
+    {
+        timer_for_next_ball=2;
+        definepower=32;
+        definespeed=80;
+    }
 }
 
 /*
