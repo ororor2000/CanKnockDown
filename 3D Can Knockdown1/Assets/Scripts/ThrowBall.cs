5084c44c8bbf1cbf23e5fc107f654549bd37e871
Or Shtrauch <ororor2000@gmail.com>
2017-12-12 18:35:05 +0200

    Fuck

--- 3D Can Knockdown1/Assets/Scripts/ThrowBall.cs
+++ 3D Can Knockdown1/Assets/Scripts/ThrowBall.cs
@@ -1,16 +1,70 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
-public class ThrowBall : MonoBehaviour {
+public class ThrowBall : MonoBehaviour
+{
+    private Touch begining;
+    private Touch end;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private bool clearToThrow = true;
+    private GameObject ball;
+
+    private float startTime;
+    private float deltaTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        ball = GameObject.Find("Ball");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                begining = Input.GetTouch(0);
+                Debug.Log("Began: " + begining.position);
+
+                startTime = Time.time;
+            }
+
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                end = Input.GetTouch(0);
+                Debug.Log("End: " + end.position);
+
+                deltaTime = Time.time - startTime;
+            }
+        }
+
+        Vector2 zero = new Vector2(0f, 0f);
+
+        if (clearToThrow && begining.position != zero && end.position != zero)
+        {
+            Rigidbody rigid = ball.GetComponent<Rigidbody>();
+
+            float angle = Vector3.Angle(begining.position, end.position);
+            float dis = Vector3.Distance(begining.position, end.position);
+
+            float velocity = dis / deltaTime;
+
+            float vx = velocity * (float)Math.Cos(angle);
+            float vy = velocity * (float)Math.Sin(angle);
+
+            Vector3 velocityVector = new Vector3(vx, vy);
+
+            velocityVector *= Time.deltaTime;
+
+            rigid.velocity = velocityVector;
+
+            clearToThrow = false;
+        }
+    }
 }
